3c219e4086b7855c188e5ca097798d49fc94a4f7
Vyacheslav <viparamount@gmail.com>
2019-08-31 23:08:22 +0300

    added prev, next button, added page switch

---
+++ pagination.js
@@ -0,0 +1,22 @@
+Vue.component('pagination', {
+	template: '#pagination-template',
+	props: {
+		current: {
+			type: Number,
+			default: 1
+		}
+	},
+	computed: {
+		nextPage: function() {
+			return this.current +1
+		},
+		prevPage: function() {
+			return this.current - 1
+		}
+	},
+	methods: {
+		changePage: function(page) {
+			this.$emit('page-changed', page)
+		}
+	}
+})
